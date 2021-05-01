f022b4195d841e304bc4866697e4ba6214d83654
JSmith604 <jacquelinesmith190@gmail.com>
2021-05-01 21:28:50 +0000

    Add open weather information

---
+++ app.js
@@ -0,0 +1,1 @@
+const url = "api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5c1e17a0931a9996eba6c80c3674b88"
--- express_server.js
+++ express_server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// const main = require ("./app");
+
+//open weather info
+const weatherUrl = "api.openweathermap.org/data/2.5/weather"
+const apiKey = "25c1e17a0931a9996eba6c80c3674b88"
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.get("/weather", (req, res) => {
   res.status(200).send({
@@ -13,6 +21,11 @@ app.get("/weather", (req, res) => {
     city: 'Vancouver'
   })
 });
+
+app.get("/url", (req, res) => {
+  res.send("Hello!");
+});
+
 
 app.listen(PORT, () => { //see if server is listening in terminal
   console.log(`Listening on port ${PORT}!`);

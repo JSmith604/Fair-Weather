f972f67cf43e99e5d5498d26c99f69a77931e827
JSmith604 <jacquelinesmith190@gmail.com>
2021-05-01 23:45:35 +0000

    delete old api that has been exposed, fix mistake by creating new api key and hiding in a .env file in gitignore

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+require('dotenv').config; //get env data
 
+const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=London,uk"
+apiKey = process.env.API_KEY
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -14,6 +17,25 @@ app.get("/weather", (req, res) => {
   })
 });
 
+app.get("/forecast", (req, res) => {
+  const forecast = async () => {
+    const urlToFetch = `${weatherUrl}&appid=${apiKey}`;
+    try {
+      const response = await fetch(urlToFetch);
+        if(response.ok) {
+          const jsonResponse = await response.json();
+          console.log(jsonResponse);
+          return jsonResponse;
+        }
+      }
+    catch(error) {
+      console.log(error);
+      }
+    }
+    console.log(forecast)
+  });
+  
 app.listen(PORT, () => { //see if server is listening in terminal
   console.log(`Listening on port ${PORT}!`);
 });
+

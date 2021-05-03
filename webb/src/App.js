7979d0e9a805cc4a680279e5335f14fd5570206f
JSmith604 <jacquelinesmith190@gmail.com>
2021-05-03 21:09:01 +0000

    Add components

--- webb/src/App.js
+++ webb/src/App.js
@@ -4,17 +4,20 @@ import websiteLogo from './websiteLogo.png';
 import './App.css';
 import { Button, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Wrapper from "./components/wrapper";
 
 function App() {
   return (
+   <Wrapper>
     <div className="App">
       <header className="App-header">
         <img src={websiteLogo} alt="logo" />
-        <Alert varient="primary">Check out the forecast for your city</Alert>
+        <Alert variant="primary">Check out the forecast for your city</Alert>
         <Button>See Weather</Button>
       </header>
     </div>
-  );
+  </Wrapper>
+  )
 }
 
 export default App;

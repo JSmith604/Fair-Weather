3faf72a517ffc4659964c848358e40260b9d715b
JSmith604 <jacquelinesmith190@gmail.com>
2021-05-03 18:40:24 +0000

    Change background colour

--- webb/src/App.js
+++ webb/src/App.js
@@ -2,26 +2,16 @@ import React from 'react';
 // import logo from './logo.svg';
 import websiteLogo from './websiteLogo.png';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import { Button, Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={websiteLogo} alt="logo" />
+        <Alert varient="primary">Check out the forecast for your city</Alert>
         <Button>See Weather</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

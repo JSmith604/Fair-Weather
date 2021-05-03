44a751a42eab0e3372f7c4667b35f2de05cf7992
JSmith604 <jacquelinesmith190@gmail.com>
2021-05-03 18:12:17 +0000

    Add logo and button

--- webb/src/App.js
+++ webb/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import websiteLogo from './websiteLogo.png';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={websiteLogo} alt="logo" />
+        <Button>See Weather</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

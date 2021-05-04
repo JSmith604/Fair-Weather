9d70de33371c0ce5367b7d56e1e060ac7e9a2c1e
JSmith604 <jacquelinesmith190@gmail.com>
2021-05-04 04:33:53 +0000

    Set form value to state

--- webb/src/App.js
+++ webb/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import websiteLogo from './websiteLogo.png';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Wrapper from "./components/wrapper";
-import Main from "./components/wrapper";
+import Main from "./components/main";
+
+import axios from 'axios';
+
+
 
 function App() {
+  const [city, setCity] = useState('');
+  // const api = axios.create({
+  //   baseURL: `http://localhost:8080/forecast?q=`
+  // })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const value = city;
+    console.log({ value });
+    // axios.get(`http://localhost:8080/forecast?q=${value}`)
+
+  }
+
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  }
+
   return (
    <Wrapper>
     <div className="App">
       <header className="App-header">
         <img src={websiteLogo} alt="logo"/>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Label>Location</Form.Label>
-              <Form.Control type="text" placeholder="Enter your city"/>
+              <Form.Control type="text" onChange={handleChange} placeholder="Enter your city"/>
             </Form.Group>
+            <Button type="submit">See Weather</Button>
           </Form>
-        <Button>See Weather</Button>
       </header>
     </div>
     <Main/>
